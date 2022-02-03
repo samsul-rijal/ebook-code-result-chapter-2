63ce74bd57f2a13b030b2a1497dab0c927d1a509
= <=>
2022-02-03 21:52:17 +0700

    day 1

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', function (req, res) {
+    res.send('Hello World');
+});
+
 const port = 5000;
 app.listen(port, function () {
     console.debug(`Server running on port ${port}`);
