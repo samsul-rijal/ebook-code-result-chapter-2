e76ce44df232cddd06fc02e24aa36cae54f132b5
= <=>
2022-02-03 23:33:55 +0700

    day 2

--- index.js
+++ index.js
@@ -5,9 +5,10 @@ const app = express()
 app.set('view engine', 'hbs');
 
 app.use('/public', express.static(__dirname + '/public'));
+app.use(express.urlencoded({ extended: false }))
 
 app.get('/', function (req, res) {
-    res.send("Hello World")
+    res.render('index')
 })
 
 app.get('/home', function (req, res) {
@@ -22,10 +23,12 @@ app.get('/blog', function (req, res) {
     })
 })
 
+app.get('/add-blog', function (req, res) {
+    res.render("form-blog")
+})
+
 app.get('/blog/:id', function (req, res) {
-    // get selected blog id with params
     const blogId = req.params.id
-    // render blog-detail page and send data to view
     res.render('blog-detail', {
         blog: {
             id: blogId,
@@ -33,21 +36,23 @@ app.get('/blog/:id', function (req, res) {
             post_date: '12 Jul 2021 22:30 WIB',
             author: 'Ichsan Emrald Alamsyah',
             content: `Ketimpangan sumber daya manusia (SDM) di sektor digital masih
-                  menjadi isu yang belum terpecahkan. Berdasarkan penelitian
-                  ManpowerGroup, ketimpangan SDM global, termasuk Indonesia,
-                  meningkat dua kali lipat dalam satu dekade terakhir. Lorem ipsum,
-                  dolor sit amet consectetur adipisicing elit. Quam, molestiae
-                  numquam! Deleniti maiores expedita eaque deserunt quaerat! Dicta,
-                  eligendi debitis?`
+                        menjadi isu yang belum terpecahkan. Berdasarkan penelitian
+                        ManpowerGroup, ketimpangan SDM global, termasuk Indonesia,
+                        meningkat dua kali lipat dalam satu dekade terakhir. Lorem ipsum,
+                        dolor sit amet consectetur adipisicing elit. Quam, molestiae
+                        numquam! Deleniti maiores expedita eaque deserunt quaerat! Dicta,
+                        eligendi debitis?`
         }
     })
 })
 
-app.get('/contact-me', function (req, res) {
-    res.render('contact')
+app.post('/blog', function (req, res) {
+    console.log({
+        title: req.body.title,
+        content: req.body.content
+    })
 })
 
-const port = 5000
-app.listen(port, function () {
-    console.debug(`Server running on port ${port}`)
+app.listen(5000, function () {
+    console.log('server starting on PORT: 5000')
 })
