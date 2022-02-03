6f0893b784d5d2c56071bfa32e66e54751db102f
= <=>
2022-02-03 22:21:32 +0700

    day 2

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
-const express = require('express');
+const express = require('express')
 
-const app = express();
+const app = express()
+
+app.set('view engine', 'hbs');
+
+app.use('/public', express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
-    res.send('Hello World');
-});
+    res.send("Hello World")
+})
 
-const port = 5000;
+app.get('/home', function (req, res) {
+    res.render('index')
+})
+
+const port = 5000
 app.listen(port, function () {
-    console.debug(`Server running on port ${port}`);
-});
+    console.debug(`Server running on port ${port}`)
+})
