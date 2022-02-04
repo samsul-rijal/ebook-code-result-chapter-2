9f96f121ce9d3c85bd4c84af3017a0af37283cd5
= <=>
2022-02-04 16:33:46 +0700

    day 2

--- api/index.js
+++ api/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
+const path = require("path");
 
 const app = express()
 
 app.set('view engine', 'hbs');
+app.set("views", path.join(__dirname, "../views"));
 
 app.use('/public', express.static(__dirname + '/public'));
 
@@ -11,6 +13,7 @@ app.get('/', function (req, res) {
 })
 
 app.get('/home', function (req, res) {
+    setHeader(res)
     res.render('index')
 })
 
@@ -18,3 +21,8 @@ const port = 5000
 app.listen(port, function () {
     console.debug(`Server running on port ${port}`)
 })
+
+function setHeader(res) {
+    res.setHeader("Content-Type", "text/html");
+    res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
+}
