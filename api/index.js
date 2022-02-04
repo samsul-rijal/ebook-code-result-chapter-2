8c00ef074b9b2c833bf7d75716e6496a0fde0bdb
= <=>
2022-02-04 16:49:00 +0700

    day 2

--- api/index.js
+++ api/index.js
@@ -1,24 +1,29 @@
 const express = require('express')
+const path = require("path");
 
 const app = express()
 
 app.set('view engine', 'hbs');
+app.set("views", path.join(__dirname, "../views"));
 
-app.use('/public', express.static(__dirname + '/public'));
+app.use("/public", express.static(path.join(__dirname, "../public")));
 
 app.get('/', function (req, res) {
     res.send("Hello World")
 })
 
 app.get('/home', function (req, res) {
+    setHeader(res)
     res.render('index')
 })
 
 app.get('/blog', function (req, res) {
+    setHeader(res)
     res.render('blog')
 })
 
 app.get('/contact-me', function (req, res) {
+    setHeader(res)
     res.render('contact')
 })
 
@@ -26,3 +31,8 @@ const port = 5000
 app.listen(port, function () {
     console.debug(`Server running on port ${port}`)
 })
+
+function setHeader(res) {
+    res.setHeader("Content-Type", "text/html");
+    res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
+}
