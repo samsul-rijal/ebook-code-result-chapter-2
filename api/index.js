3bbce1dccdcba8b98fc59bcc453210ea1232c360
= <=>
2022-02-04 16:38:18 +0700

    day 2

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ const app = express()
 app.set('view engine', 'hbs');
 app.set("views", path.join(__dirname, "../views"));
 
-app.use('/public', express.static(__dirname + '/public'));
+app.use("/public", express.static(path.join(__dirname, "../public")));
 
 app.get('/', function (req, res) {
     res.send("Hello World")
