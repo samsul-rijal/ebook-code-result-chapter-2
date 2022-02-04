30e10f031d007eb72013a2dfa5ba5212128105b7
= <=>
2022-02-04 11:54:36 +0700

    update deploy vercel

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', function (req, res) {
+    setHeader(res)
     res.send('Hello World');
 });
 
@@ -10,3 +11,8 @@ const port = 5000;
 app.listen(port, function () {
     console.debug(`Server running on port ${port}`);
 });
+
+function setHeader(res) {
+    res.setHeader("Content-Type", "text/html");
+    res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
+}
