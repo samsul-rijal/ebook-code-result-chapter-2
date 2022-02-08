4c1579d6e2030fbd37ef2946caa5da4fdfaca0fe
= <=>
2022-02-08 23:26:54 +0700

    day 7

---
+++ middlewares/uploadFile.js
@@ -0,0 +1,14 @@
+const multer = require('multer')
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "uploads") // file storage location
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + "-" + file.originalname) // rename filename by => date now + original filename
+    }
+})
+
+const upload = multer({ storage: storage })
+
+module.exports = upload
